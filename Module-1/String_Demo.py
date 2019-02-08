ee53979e26df927baac6a6591ea8f00aabcf4c4e
Deepak Agrawal <agrawal.deepak22@gmail.com>
2019-02-08 13:12:10 +0530

    Repository Updated

---
+++ Module-1/String_Demo.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  8 13:09:48 2019
+
+@author: Deepak
+"""
+
--- Module-1/WelcomeDemo.py
+++ Module-1/WelcomeDemo.py
@@ -1,9 +1,12 @@
 #initialize the variables
 lname=fname=gender=name=''
+#Printing The Welcome Message
 print("Welcome to Cafe Coffee Shop\n")
 print("Please provide your following Information\n")
+#Information of the user
 lname=input("Please tell me your last Name.....")
 fname=input("Please tell me your firsat name")
 gender=input("Please tell me your gender")
 name=input("Tell /me to whom you want to meet?")
+#printing the waiting message to the user
 print("Please take your seat Mr. {}{}. Mr. {} is busy in meeting till then please take a cup of coffee.". format(fname,lname, name))
